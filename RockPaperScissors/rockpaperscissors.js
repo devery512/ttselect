7459f052326de30f77a8308e37a112cf38f7d32c
Garrian BrownE <garrianbrown@gmail.com>
2019-04-26 09:30:45 -0500

    changed certain console.logs to alert functions.

--- RockPaperScissors/rockpaperscissors.js
+++ RockPaperScissors/rockpaperscissors.js
@@ -1,29 +1,26 @@
 // -----------------------New rock paper scissor game---------------------
-// alert("Welcome to Rock, Paper, Scissors.")
 var hands = ['rock','paper','scissors']
 var computerChoice = gethand();
 var playerChoiceRock = hands[0];
 var playerChoicePaper = hands[1];
 var playerChoiceScissors = hands[2];
-
-var plays = {
-	playerHand: {
-		rock: hands[0],
-		paper: hands[1],
-		scissors: hands[2],
-	}
-}
+// var plays = {
+// 	playerHand: {
+// 		rock: hands[0],
+// 		paper: hands[1],
+// 		scissors: hands[2],
+// 	}
+// }
 
 //------ Functions --------------------
 	function gethand(x)
 			{
 				var x = parseInt((Math.random()*3))
-				if (x == 0 )
+				if (x === 0 )
 					{
 						x = hands[0];
-
 					}
-				else if(x == 1)
+				else if(x === 1)
 					{
 						x = hands[1];
 					}
@@ -32,7 +29,6 @@ var plays = {
 						x = hands[2];
 					}
 				return x;
-				
 			};
 
 	function player(playerChoice){
@@ -57,27 +53,25 @@ var plays = {
 		console.log("Player's choice: " + playerChoice);
 	};
 
-
 	function compare(a,b)
 	{
 	if (a == b)
 	 {
-	 	console.log("its a tie");
+	 	alert("IT'S A TIE");
 	 }
 	 else if((a == "rock" && b == "scissors") || (a == "paper" && b == "rock") || (a == "scissors" && b =="paper"))
 	 {
-	 	console.log("player wins");
+	 	alert("PLAYER WINS");
 	 }
 	 else if((b == "rock" && a == "scissors") || (b == "paper" && a == "rock") || (b == "scissors" && a =="paper"))
 	 {
-	 	console.log("computer wins");
+	 	alert("COMPUTER WINS");
 	 }
 	};
 
 // console.log("Computer's choice: " + computerChoice);
 // // player(playerChoice);
 // compare(computerChoice,playerChoice);
-
 
 //------------- Javascript to HTML -------------
 
@@ -86,7 +80,6 @@ document.getElementById("rock").onclick = function(){
 	player(playerChoiceRock);
 	console.log("Computer's Choice: "+ cchoice);
 	compare(playerChoiceRock, cchoice);
-	alert();
 };
 
 document.getElementById("paper").onclick = function(){
